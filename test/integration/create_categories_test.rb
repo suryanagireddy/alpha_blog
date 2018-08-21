670518372053bce57c93babaaf5f9126ae081e87
Surya <nagireddysurya7@gmail.com>
2018-08-21 14:42:39 +0000

    Add categories to nav bar and admin control on category tests

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
      
+     def setup
+        @user = User.create(username: "john", email: "john@gmail.com", password: "password", admin: true)
+     end 
+     
+     
      test "get new category form and create category" do
+         sign_in_as(@user, "password")
          get new_category_path
          assert_template 'categories/new'
          assert_difference 'Category.count', 1 do
@@ -14,7 +20,8 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         end 
      
       test "invalid category submission results in failure" do
-        get new_category_path
+         sign_in_as(@user, "password")
+         get new_category_path
          assert_template 'categories/new'
          assert_no_difference 'Category.count' do
              post categories_path, params: {category: {name: " "}}
